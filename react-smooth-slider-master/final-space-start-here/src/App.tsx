a4e7f0149b07a22a7bf60380cd5779a2be3466f4
DouglasMacKrell <douglasmackrell@pursuit.org>
2020-10-20 16:30:15 -0400

    api query and slider complete

--- react-smooth-slider-master/final-space-start-here/src/App.tsx
+++ react-smooth-slider-master/final-space-start-here/src/App.tsx
@@ -3,7 +3,7 @@ import Dialog from '@material-ui/core/Dialog';
 // Components
 import Slider from './components/Slider';
 
-const Sliderprops = {
+const SliderProps = {
   zoomFactor: 30, // How much the image should zoom on hover in percent
   slideMargin: 10, // Margin on each side of slides
   maxVisibleSlides: 5,
@@ -25,7 +25,45 @@ type Character = {
 };
 
 const App: React.FC = () => {
-  return <div>Final Space App</div>;
+  const [data, setData] = useState<Character[]>([]);
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+  const [activeCharacter, setActiveCharacter] = useState<Character>();
+
+  const handleDialogOpen = (character: Character) => {
+    setIsDialogOpen(true)
+    setActiveCharacter(character)
+  }
+
+  useEffect(() => {
+    const getData = async () => {
+      const data = await (
+        await fetch('https://finalspaceapi.com/api/v0/character/')
+      ).json();
+      setData(data);
+    };
+    getData();
+  }, [])
+
+  console.log(data)
+
+  if (data.length < 1) return <div>Loading...</div>
+
+  return (
+    <>
+      <Dialog onClose={() => setIsDialogOpen(false)} open={isDialogOpen} >
+        <div>Content</div>
+      </Dialog>
+
+      <Slider {...SliderProps} >
+        {data.map(character => (
+          <div key={character.id} onClick={() => handleDialogOpen(character)} >
+            <img src={character.img_url} alt="character" />
+          </div>
+        ))}
+      </Slider>
+    </>  
+  );
 };
+
 
 export default App;
