662a59e9d705dc613cda4af0761facf494bd618b
DouglasMacKrell <douglasmackrell@pursuit.org>
2020-10-20 19:08:45 -0400

    slider complete and files cleaned

--- react-smooth-slider-master/final-space-start-here/src/components/CharacterCard.tsx
+++ react-smooth-slider-master/final-space-start-here/src/components/CharacterCard.tsx
@@ -29,6 +29,30 @@ const CharacterCard: React.FC<Props> = ({ character }) => {
     return (
         <Card className={classes.root}>
             <CardMedia className={classes.media} image={character.img_url} />
+            <CardContent>
+                <Typography gutterBottom variant="h5">
+                    Name: {character.name}
+                </Typography>
+                <Typography gutterBottom color="textSecondary">
+                    Hair: {character.hair}
+                    <br />
+                    Origin: {character.origin}
+                    <br />
+                    Species: {character.species}
+                    <br />
+                    Status: {character.status}
+                    <br />
+                    Alias:
+                    <br />
+                </Typography>
+                {character.alias.map((alias: any) => (
+                    <Chip key={alias} style={{ margin: 3 }} size="small" label={alias} />
+                ))}
+                <Typography color="textSecondary">Abilities:</Typography>
+                {character.abilities.map((ability: any) => (
+                    <Chip key={ability} style={{ margin: 3 }} size="small" label={ability} />
+                ))}
+            </CardContent>
         </Card>
     )
 }
